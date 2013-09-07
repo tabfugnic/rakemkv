4c6c75dfb30954ca945e713e1b1ed76553f8cfd8
Eric Collins <tabfugnic@gmail.com>
2013-09-07 17:51:04 -0400

    Comments and cleanup to the title object

--- lib/rakemkv/title.rb
+++ lib/rakemkv/title.rb
@@ -4,6 +4,12 @@ module RakeMKV
     attr_accessor :id, :cells
     attr_reader :time
 
+    MINUTE = 60
+    HOUR = 3600
+
+    ##
+    # Constructor for title
+    #
     def initialize(id, time, cells, code=nil)
       @id = id.to_i
       @time =  convert_to_sec(time)
@@ -11,20 +17,30 @@ module RakeMKV
       @code = code
     end
 
+    ##
+    # Setter for time, converting string to seconds
+    #
     def time=(time)
       @time = convert_to_sec(time)
     end
 
+    ##
+    # Find short lengthed title
+    #
     def short_length?
       return time > 900 && time < 2100
     end
 
-    protected
+    private
 
+    ##
+    # Convert string of time in format HH:MM:SEC
+    # to integer representing time in seconds
+    #
     def convert_to_sec(time)
       return time if time.is_a? Integer
       times = time.split(":")
-      return (times[0].to_i * 3600) + (times[1].to_i * 60) + (times[2].to_i)
+      return (times[0].to_i * HOUR) + (times[1].to_i * MINUTE) + (times[2].to_i)
     end
   end
 end
